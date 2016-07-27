9050f72ce76de15a79995421045ca788a7fb87b4
Joshua Arnold <arnold-jr@users.noreply.github.com>
2016-07-26 23:00:56 -0400

    Not working but CV framework implemented.

--- cleandata/core.py
+++ cleandata/core.py
@@ -1,28 +1,116 @@
 import pandas as pd
 import numpy as np
 import helpers as h
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.linear_model import Ridge
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import KFold, ShuffleSplit
 
-# df_meta = pd.read_csv("./resources/Metadata.csv")
-# print df_meta
-def clean(fpath):
-  df = pd.read_csv(fpath, index_col=None)
 
-  # Drop any rows that don't have a valid loan rate value
-  df = df.loc[df["X1"].isnull() == False, :]
+def write_clean_store(fpath, train=True):
+  """ Gets the dirty database.
 
-  df = h.CleanerTransformer().transform(df)
+  :param fpath: file path corresponding to the csv input file
+  :return path to the store
+  """
+  storePath = fpath.replace(".csv",".h5")
 
-  df = h.VectorizerTransformer().fit_transform(df)
+  cleanerTransformer = h.CleanerTransformer()
+  store = pd.HDFStore(storePath, mode='w')
+  for chunk in pd.read_csv(fpath, chunksize=40000):
+    if train:
+      # Drop any rows that don't have a valid loan rate value
+      chunk = chunk.loc[chunk["X1"].isnull() == False, :]
+    chunk = cleanerTransformer.transform(chunk)
+    print chunk
+    store.append('df', chunk)
+  store.close()
 
-  for cols in list(h.chunks(df.columns.tolist(), 10)):
-    print df[cols].head()
+  return storePath
 
-def create_model():
-  pass
 
+def train_model(fpath, modelChoice):
+  # chunk = pd.read_csv(fpath, converters={'X1':h.pct_convert})
+  chunk = pd.read_csv(fpath,
+                      parse_dates=["X15","X23"],
+                      date_parser=lambda x:pd.to_datetime(x, format='%b-%y'))
+  print chunk.info()
+  print chunk.loc[:,['X15','X23']].head()
+
+  # Drop NaNs for now
+  chunk = h.CleanerTransformer().transform(chunk)
+  print chunk.info()
+
+
+  numeric = (['X' + str(n) for n in xrange(2, 7)] +
+                   ['X13', 'X15'] +
+                   ['X' + str(n) for n in xrange(21, 32)]
+                   )
+  categorical = ["X7", "X8", "X9", "X11", "X12", "X14", "X17", "X19", "X20"]
+
+  print h.ColumnSelectorTransformer(numeric).transform(
+    h.CleanerTransformer().transform(chunk)).info()
+
+  # [('numeric', h.ColumnSelectorTransformer(numeric))] +
+
+  model_pipe = Pipeline([
+    ('clean', h.CleanerTransformer()),
+    ('features', FeatureUnion(
+      list((c, h.ColumnHasherTransformer(c)) for c in categorical)
+    )),
+    ('model', modelChoice['model']),
+  ])
+
+  y = chunk.loc[:,'X1']
+  print y.isnull().sum()
+
+
+  cv = KFold(len(y), n_folds=10, shuffle=True, random_state=42)
+  # cv = ShuffleSplit(len(y), n_iter=10, test_size=0.3)
+  print cv
+
+  param_grid = dict(model__alpha = np.logspace(-1,1,3))
+  rgr_CV = GridSearchCV(model_pipe,
+                        param_grid={'model__'+k:v
+                                    for k,v in
+                                    modelChoice['param_grid'].iteritems()},
+                        cv=cv,
+                        n_jobs=-1)
+  rgr_CV.fit(chunk, y)
+
+  print("Best params", rgr_CV.best_params_)
+  print("Best score", rgr_CV.best_score_)
+
+  return (rgr_CV.best_params_)
 
 
 if __name__ == "__main__":
-  fpath = "../resources/dirtySub.csv"
-  # fpath = "../resources/Data for Cleaning & Modeling.csv"
-  clean(fpath)
+  if 1:
+    fpath = "../resources/dirtySub.csv"
+  else:
+    fpath = "../resources/Data for Cleaning & Modeling.csv"
+
+  if 0:
+    with h.stopwatch('storing hdf'):
+      storePath = write_clean_store(fpath, train=True)
+
+
+  modelChoices = dict(ridge =
+                   dict(model=Ridge(),
+                        param_grid={'alpha': np.logspace(-3,3,7)}
+                        ),
+                   knn =
+                   dict(model=KNeighborsRegressor(n_jobs=1),
+                        param_grid={'n_neighbors': range(1,10,2)}
+                        ),
+                   rff =
+                   dict(model=RandomForestRegressor(n_jobs=1),
+                        param_grid={'max_depth': [10, 50, 100]}
+                        ),
+                   )
+
+  with h.stopwatch('training model'):
+    train_model(fpath, modelChoices['knn'])
+
--- cleandata/helpers.py
+++ cleandata/helpers.py
@@ -1,13 +1,32 @@
-from itertools import repeat
+import contextlib
+import time
 import numpy as np
 import pandas as pd
-from decimal import Decimal
 import re
+from decimal import Decimal
 import locale
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction import FeatureHasher, DictVectorizer
+import scipy
+
+from sklearn import cross_validation
+from sklearn import neighbors
+from sklearn import grid_search
+
 
 locale.setlocale( locale.LC_ALL, 'en_CA.UTF-8' )
+
+
+@contextlib.contextmanager
+def stopwatch(message):
+  """Context manager that prints how long a block takes to execute."""
+  t0 = time.time()
+  try:
+    yield
+  finally:
+    t1 = time.time()
+  print('Total elapsed time for %s: %.3f s' % (message, t1 - t0))
+
 
 
 def chunks(l, n):
@@ -21,7 +40,7 @@ def chunks(l, n):
     yield l[i:i + n]
 
 def convert(func):
-  """ Wraps convert functions to return NaN on errors
+  """ Wraps convert functions to return NaN on errors.
 
   :param func: the function to be mapped
   :return func wrapped with error handling
@@ -34,117 +53,174 @@ def convert(func):
   return func_wrapper
 
 
-@convert
+# @convert
 def pct_convert(s):
-  """ Converts a percentage to a float
+  """ Converts a percentage string to a float.
 
   :param s: string representation of a percentage
   :return percentage value as float
   """
-  return np.float(s.rstrip('%'))
+  try:
+    return np.float(s.rstrip('%'))
+  except:
+    return np.nan
 
-
-@convert
+# @convert
 def dollar_convert(s):
-  v = Decimal(dollar_convert.re_dollar_stripper.sub('',s))
-  s1 = locale.currency(v, grouping=True)
-  if s1 != s and s1[:-3] != s:
-    raise ValueError("%s not equal to %s" % (s1, s))
-  return v
+  """ Converts a dollar amount string to a float.
+
+  :param s: string representation of a dollar amount
+  :return dollar amount as float
+  """
+  try:
+    v = Decimal(dollar_convert.re_dollar_stripper.sub('',s))
+    s1 = locale.currency(v, grouping=True)
+    if s1 != s and s1[:-3] != s:
+      raise ValueError("%s not equal to %s" % (s1, s))
+    return v
+  except:
+    return np.nan
 setattr(dollar_convert,'re_dollar_stripper', re.compile(r'[^0-9\.]'))
 
 
-@convert
+# @convert
 def init_listing_convert(s):
-  if s.lower() in ['s','f']:
-    return s.lower()
-  else:
-    return np.nan
+  """ Converts initial listing to an integer representation.
+
+  :param s: input string of either 's' or 'f'
+  :return integer in the range [-1,1]
+  """
+  try:
+    if s.lower() == 's':
+      return -1
+    elif s.lower() == 'f':
+      return 1
+    else:
+      return 0
+  except:
+    return 0
 
 
-@convert
+# @convert
 def pos_int_convert(n):
+  """ Enforces strictly positive integer input.
+
+  :param n: the number to be converted
+  :return the integer n if strictly positive, otherwise NaN
+  """
   if n > 0:
-    return n
+    return np.int16(n)
   else:
-    return np.nan
+    return np.int16(-1)
 
 
-@convert
+# @convert
 def date_convert(d):
-  return pd.to_datetime(d)
+  """ Converts a date string into datetime format.
 
+  :param d: date string
+  :return datetime representation"""
+  try:
+    return pd.to_datetime(d)
+  except:
+    return pd.to_datetime(np.nan)
 
+# @convert
+def has_text_convert(s):
+  """ Identifies whether the input contains text
+
+  :param s: input to verify
+  :return Boolean indicating True if text is present
+  """
+  if str(s) == 'nan':
+    return False
+  else:
+    return True
 
 
 class CleanerTransformer(BaseEstimator, TransformerMixin):
 
   def __init__(self):
     self.converters = [
-      (["X01", "X30"], pct_convert, np.float64),
-      (["X04", "X05", "X06"], dollar_convert, np.float64),
-      (["X32"], init_listing_convert, np.object_),
+      (["X1", "X30"], pct_convert, np.float64),
+      (["X4", "X5", "X6"], dollar_convert, np.float64),
+      (["X32"], init_listing_convert, np.int8),
       (["X22"] + ["X" + str(d) for d in xrange(24,29)]+["X31"], pos_int_convert,
-       np.uint16),
-      (["X15","X23"], date_convert, np.datetime_data)
+       np.int16),
+      (["X16","X18"], has_text_convert, np.bool)
     ]
     pass
 
-  def fit(self, X, y):
+  def fit(self, X, y=None):
     return self
 
   def transform(self, df):
-    """ Transforms dirty input DataFrame into a cleaned DataFrame
+    """ Transforms dirty input DataFrame into a cleaned DataFrame.
 
     :param df: pandas DataFrame
-    :return cleaned DataFrame
+    :return nd-array
     """
-
-    # Pad axis names with zeroes
-    df.rename_axis(lambda s: 'X' + s.lstrip("X").zfill(2), axis=1, inplace=True)
-
-    # Convert elements
+    true_columns = df.columns.values
     for c in self.converters:
-      cols, func, _ = c
-      df[cols] = df[cols].applymap(func)
-
-    return df
-
+      cols, func, dtype = c
+      for col in cols:
+        if col in true_columns:
+          if dtype is not None:
+            df.loc[:,col] = df.loc[:,col].apply(func)
+          else:
+            df.loc[:,col] = df.loc[:,col].apply(func)
+    return df.dropna(axis=0)
 
 
 class VectorizerTransformer(BaseEstimator, TransformerMixin):
 
   def __init__(self):
-    self.colVecDict = dict(zip(
-      ["X07", "X08", "X09", "X11", "X12", "X14", "X17"],
-      repeat(DictVectorizer(sparse=False))))
+    """ Initializes dict of columns to be vectorized and their vectorizers. """
+    self.categorical = ["X7", "X8", "X9", "X11", "X12", "X14", "X17", "X19",
+                        "X20"]
+    self.colVecDict = {}
+    for x in self.categorical:
+      self.colVecDict.update({x:FeatureHasher(n_features=1024,
+                                              input_type='dict')})
     pass
 
-
-  def fit(self, df):
-    for col, vec in self.colVecDict.iteritems():
-      vec.fit(df[[col]].fillna("NA").T.to_dict().values())
+  def fit(self, X, y=None):
     return self
 
   def transform(self, df):
     """ Transforms dirty input DataFrame into a cleaned DataFrame
 
     :param df: pandas DataFrame
-    :return cleaned DataFrame
+    :return scipy sparse matrix containing hashed features
     """
-    for col, vec in self.colVecDict.iteritems():
-      X = vec.transform(df[[col]].fillna("NA").T.to_dict().values())
-      df = pd.concat([df,
-                      pd.DataFrame(X,
-                                   columns=vec.get_feature_names(),
-                                   dtype=np.uint8)],
-                     axis=1)
+    for i, (col, vec) in enumerate(self.colVecDict.iteritems()):
+      t = vec.transform(df.loc[:,col].fillna("NA")\
+                        .apply(lambda x: {x: 1}).values)
+      if i == 0:
+        sparseMatrix = t
+      else:
+        sparseMatrix = scipy.sparse.hstack((sparseMatrix,t))
 
-    df.sort_index(axis=1, inplace=True)
+    return sparseMatrix
 
-    return df
+class ColumnHasherTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, col):
+    self.col = col
+    self.fh = FeatureHasher(n_features=1024, input_type='dict')
 
+  def fit(self, X, y=None):
+    return self
 
-if __name__ == "__main__":
-  pass
+  def transform(self, df):
+    return self.fh.transform(df.loc[:,self.col].fillna("NA")\
+                             .apply(lambda x: {x: 1}).values)
 
+class ColumnSelectorTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, cols):
+    self.cols = cols
+
+  def fit(self, X, y=None):
+    return self
+
+  def transform(self, df):
+    return df.loc[:,self.cols]
